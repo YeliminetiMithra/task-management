ba06c243d0c45d958777d517ccb07ff217ec33ab
YeliminetiMithra <mithramithra123456@gmail.com>
2025-07-24 09:52:47 +0530

    Update TaskMainDetails.jsx

--- src/tasks/TaskMainDetails.jsx
+++ src/tasks/TaskMainDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useTasks } from "../context/TaskContext";
-import styles from '../App.css';
+import styles from './TaskMainDetails.css';
 
 export default function TaskMainDetails() {
   const initialForm = {
